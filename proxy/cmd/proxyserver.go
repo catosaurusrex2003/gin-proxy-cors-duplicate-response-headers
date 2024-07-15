f6d199db40cdca6208c265485ea2af93ea464d3f
catosaurusrex2003 <mohdmehdi2003@gmail.com>
2024-07-15 22:25:34 +0530

    initialisatoun

---
+++ proxy/cmd/proxyserver.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	// Configure CORS
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"}
+	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type"}
+	router.Use(cors.New(config))
+
+	// Setup proxy
+	target, _ := url.Parse("http://localhost:8080")
+	proxy := httputil.NewSingleHostReverseProxy(target)
+	router.Any("/*proxyPath", func(c *gin.Context) {
+		c.Request.URL.Path = c.Param("proxyPath")
+		proxy.ServeHTTP(c.Writer, c.Request)
+	})
+
+	// Start server
+	router.Run(":8081")
+}
---
+++ server/cmd/server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	// Configure CORS
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"}
+	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type"}
+	router.Use(cors.New(config))
+
+	// Dummy route
+	router.GET("/dummy", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello from Server 1",
+		})
+	})
+
+	// Start server
+	router.Run(":8080")
+}
